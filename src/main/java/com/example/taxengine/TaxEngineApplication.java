4871bddc771e5cf1dc60110b782fafcd2a946cf2
Hai Hoang <simplenez.gmail.com>
2021-02-18 20:18:54 -0500

    Initial commit.

---
+++ src/main/java/com/example/taxengine/TaxEngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.taxengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/taxengine/TaxEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taxengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
